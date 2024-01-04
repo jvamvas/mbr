0bdc6a00ce446cbda3719aff3a1370034e2ce887
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-04 19:31:53 +0100

    .

--- experiments/aggregate-comet/sample_fairseq.py
+++ experiments/aggregate-comet/sample_fairseq.py
@@ -11,17 +11,20 @@ language_pair = sys.argv[1]
 assert language_pair in ["de-en", "en-de", "en-ru", "ru-en"]
 
 num_samples = 256
+seed = 42
 
 src_path = sacrebleu.get_source_file("wmt19", language_pair)
 dataset = load_dataset("text", data_files={"test": src_path})
 source_sentences = dataset["test"]["text"]
 
 model = torch.hub.load('pytorch/fairseq', f'transformer.wmt19.{language_pair}.single_model')
-model.sample('Hello world')
+model.eval()
+if torch.cuda.is_available():
+    model.cuda()
 
 out_dir = Path(__file__).parent / f"samples"
 out_dir.mkdir(exist_ok=True, parents=True)
-out_path = out_dir / f"wmt19.{language_pair}.{num_samples}.unbiased.jsonl"
+out_path = out_dir / f"wmt19.{language_pair}.{num_samples}.unbiased.seed{seed}.jsonl"
 
 with jsonlines.open(out_path, "w") as f:
     for source_sentence in tqdm(source_sentences):
