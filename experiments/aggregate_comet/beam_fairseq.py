3fe4e398d84c6bdb2547bbda96af80361679562f
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-07 13:45:28 +0100

    .

--- experiments/aggregate_comet/beam_fairseq.py
+++ experiments/aggregate_comet/beam_fairseq.py
@@ -1,4 +1,3 @@
-import sys
 from pathlib import Path
 
 import sacrebleu
@@ -6,24 +5,23 @@ from datasets import load_dataset
 
 from fairseq_utils import load_model
 
-language_pair = sys.argv[1]
-assert language_pair in ["de-en", "en-de", "en-ru", "ru-en"]
-
 beam_size = 4
 
-src_path = sacrebleu.get_source_file("wmt19", language_pair)
-dataset = load_dataset("text", data_files={"test": src_path})
-source_sentences = dataset["test"]["text"]
+for wmt in ["wmt21", "wmt22"]:
+    for language_pair in  ["de-en", "en-de", "en-ru", "ru-en"]:
+        src_path = sacrebleu.get_source_file(wmt, language_pair)
+        dataset = load_dataset("text", data_files={"test": src_path})
+        source_sentences = dataset["test"]["text"]
 
-model = load_model(language_pair)
+        model = load_model(language_pair)
 
-out_dir = Path(__file__).parent / f"translations"
-out_dir.mkdir(exist_ok=True, parents=True)
-out_path = out_dir / f"{model}.beam{beam_size}.{language_pair.split('-')[1]}"
+        out_dir = Path(__file__).parent / f"translations_{wmt}"
+        out_dir.mkdir(exist_ok=True, parents=True)
+        out_path = out_dir / f"{model}.beam{beam_size}.{language_pair.split('-')[1]}"
 
-translations = model.translate(source_sentences, beam_size=beam_size)
-assert len(translations) == len(source_sentences)
+        translations = model.translate(source_sentences, beam_size=beam_size)
+        assert len(translations) == len(source_sentences)
 
-with open(out_path, "w") as f:
-    for translation in translations:
-        f.write(translation + "\n")
+        with open(out_path, "w") as f:
+            for translation in translations:
+                f.write(translation + "\n")
--- experiments/aggregate_comet/evaluate_beam.py
+++ experiments/aggregate_comet/evaluate_beam.py
@@ -13,40 +13,41 @@ comet = evaluate.load("comet", "Unbabel/wmt22-comet-da")
 
 results_file = jsonlines.open(Path(__file__).parent / f"results_beam{beam_size}.jsonl", "w")
 
-for language_pair in ["de-en", "en-de", "en-ru", "ru-en"]:
-    translations_name = f"transformer.wmt19.{language_pair}.single_model.beam{beam_size}.{language_pair.split('-')[1]}"
-    translations_path = Path(__file__).parent / "translations" / translations_name
-    assert translations_path.exists(), translations_path
-    translations = translations_path.read_text().splitlines()
+for wmt in ["wmt21", "wmt22"]:
+    for language_pair in ["de-en", "en-de", "en-ru", "ru-en"]:
+        translations_name = f"transformer.wmt19.{language_pair}.single_model.beam{beam_size}.{language_pair.split('-')[1]}"
+        translations_path = Path(__file__).parent / "translations" / translations_name
+        assert translations_path.exists(), translations_path
+        translations = translations_path.read_text().splitlines()
 
-    src_path = sacrebleu.get_source_file("wmt19", language_pair)
-    ref_path = sacrebleu.get_reference_files("wmt19", language_pair)[0]
-    dataset = load_dataset("text", data_files={"test": src_path})
-    references = Path(ref_path).read_text().splitlines()
-    source_sequences = dataset["test"]["text"]
-    assert len(dataset["test"]) == len(references) == len(source_sequences) == len(translations)
+        src_path = sacrebleu.get_source_file("wmt19", language_pair)
+        ref_path = sacrebleu.get_reference_files("wmt19", language_pair)[0]
+        dataset = load_dataset("text", data_files={"test": src_path})
+        references = Path(ref_path).read_text().splitlines()
+        source_sequences = dataset["test"]["text"]
+        assert len(dataset["test"]) == len(references) == len(source_sequences) == len(translations)
 
 
-    chrf_score = chrf.compute(
-        predictions=translations,
-        references=references,
-    )
-    cometinho_score = cometinho.compute(
-        predictions=translations,
-        references=references,
-        sources=source_sequences,
-    )
-    comet22_score = comet.compute(
-        predictions=translations,
-        references=references,
-        sources=source_sequences,
-    )
-    results_file.write({
-        "language_pair": language_pair,
-        "method": f"beam search with beam size {beam_size}",
-        "chrf": chrf_score["score"],
-        "cometinho": 100 * cometinho_score["mean_score"],
-        "comet22": 100 * comet22_score["mean_score"],
-    })
+        chrf_score = chrf.compute(
+            predictions=translations,
+            references=references,
+        )
+        cometinho_score = cometinho.compute(
+            predictions=translations,
+            references=references,
+            sources=source_sequences,
+        )
+        comet22_score = comet.compute(
+            predictions=translations,
+            references=references,
+            sources=source_sequences,
+        )
+        results_file.write({
+            "language_pair": language_pair,
+            "method": f"beam search with beam size {beam_size}",
+            "chrf": chrf_score["score"],
+            "cometinho": 100 * cometinho_score["mean_score"],
+            "comet22": 100 * comet22_score["mean_score"],
+        })
 
-results_file.close()
+    results_file.close()
