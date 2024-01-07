422a04c20fdb77350fdb6774017dc79c44ab6666
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-07 13:59:30 +0100

    .

--- experiments/aggregate_comet/evaluate_beam.py
+++ experiments/aggregate_comet/evaluate_beam.py
@@ -16,12 +16,12 @@ results_file = jsonlines.open(Path(__file__).parent / f"results_beam{beam_size}.
 for wmt in ["wmt21", "wmt22"]:
     for language_pair in ["de-en", "en-de", "en-ru", "ru-en"]:
         translations_name = f"transformer.wmt19.{language_pair}.single_model.beam{beam_size}.{language_pair.split('-')[1]}"
-        translations_path = Path(__file__).parent / "translations" / translations_name
+        translations_path = Path(__file__).parent / f"translations_{wmt}" / translations_name
         assert translations_path.exists(), translations_path
         translations = translations_path.read_text().splitlines()
 
-        src_path = sacrebleu.get_source_file("wmt19", language_pair)
-        ref_path = sacrebleu.get_reference_files("wmt19", language_pair)[0]
+        src_path = sacrebleu.get_source_file(wmt, language_pair)
+        ref_path = sacrebleu.get_reference_files(wmt, language_pair)[0]
         dataset = load_dataset("text", data_files={"test": src_path})
         references = Path(ref_path).read_text().splitlines()
         source_sequences = dataset["test"]["text"]
@@ -42,6 +42,7 @@ for wmt in ["wmt21", "wmt22"]:
             sources=source_sequences,
         )
         results_file.write({
+            "testset": wmt,
             "language_pair": language_pair,
             "method": f"beam search with beam size {beam_size}",
             "chrf": chrf_score["score"],
