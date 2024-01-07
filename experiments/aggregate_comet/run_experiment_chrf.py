ae95f0b749ad0ae6287c33f7221f14ac616df40f
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-07 21:10:30 +0100

    .

--- experiments/aggregate_comet/run_experiment_chrf.py
+++ experiments/aggregate_comet/run_experiment_chrf.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from pathlib import Path
 from typing import List, Callable
@@ -9,37 +10,59 @@ from datasets import load_dataset
 
 from utils import mbr_standard_chrf, mbr_aggregate_chrf
 
+split = sys.argv[2]
+assert split in ["valid", "test"]
+
+seed_no = int(sys.argv[3])
+
+num_samples = 256
+epsilon_cutoff = 0.02
+
+if split == "valid":
+    wmt = "wmt21"
+elif split == "test":
+    wmt = "wmt22"
+else:
+    raise ValueError(split)
+
+results_file = jsonlines.open(Path(__file__).parent / f"results_chrf_{wmt}_{num_samples}samples_seed{seed_no}.jsonl", "w")
+
 chrf = evaluate.load("chrf")
 cometinho = evaluate.load("comet", "eamt22-cometinho-da")
 comet = evaluate.load("comet", "Unbabel/wmt22-comet-da")
 
-num_samples = 256
-seed = 42
-epsilon_cutoff = 0.02
-
-results_file = jsonlines.open(Path(__file__).parent / f"results_chrf.jsonl", "w")
 for language_pair in ["de-en", "en-de", "en-ru", "ru-en"]:
 
-    samples_name = f"transformer.wmt19.{language_pair}.single_model.{num_samples}samples.epsilon{epsilon_cutoff}.seed{seed}.jsonl"
-    samples_path = Path(__file__).parent / "samples" / samples_name
+    print(f"Using {split} split (={wmt}) with {num_samples} samples")
+
+    samples_dir = Path(__file__).parent / f"samples_{wmt}"
+    samples_name = f"transformer.wmt19.{language_pair}.single_model.1024samples.epsilon{epsilon_cutoff}.seed{seed_no}.jsonl"
+    samples_path = samples_dir / samples_name
     assert samples_path.exists(), samples_path
 
     samples = []  # segments x num_samples
     with jsonlines.open(samples_path) as f:
         for line in f:
-            samples.append(line["samples"][0])
+            samples.append(line["samples"])
 
-    src_path = sacrebleu.get_source_file("wmt19", language_pair)
-    ref_path = sacrebleu.get_reference_files("wmt19", language_pair)[0]
+    # Cut to desired number of samples
+    samples = [row[:num_samples] for row in samples]
+    for row in samples:
+        assert len(row) == num_samples
+    unique_sample_counts = [len(set(row)) for row in samples]
+    print(f"Average number of unique samples: {sum(unique_sample_counts) / len(unique_sample_counts):.2f}")
+
+    src_path = sacrebleu.get_source_file(wmt, language_pair)
+    ref_path = sacrebleu.get_reference_files(wmt, language_pair)[0]
     dataset = load_dataset("text", data_files={"test": src_path})
     references = Path(ref_path).read_text().splitlines()
     source_sequences = dataset["test"]["text"]
     assert len(dataset["test"]) == len(references) == len(source_sequences)
 
-    # print("Only using 20 samples for testing")
-    # samples = samples[:20]
-    # source_sequences = source_sequences[:20]
-    # references = references[:20]
+    # print("Only using 16 samples for testing")
+    # samples = samples[:16]
+    # source_sequences = source_sequences[:16]
+    # references = references[:16]
 
     def do_evaluate(method: str, select_func: Callable[[List[str]], List[str]]):
         time_start = time.time()
