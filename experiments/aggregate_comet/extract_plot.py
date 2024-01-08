4a04a834d26b0379676281d867b07aaee6fac20c
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-08 21:52:56 +0100

    .

--- experiments/aggregate_comet/extract_plot.py
+++ experiments/aggregate_comet/extract_plot.py
@@ -8,7 +8,13 @@ jsonl_path = Path(sys.argv[1])
 with jsonlines.open(jsonl_path) as f:
     data = [line for line in f]
 
-beam_search = float(sys.argv[2])
+baseline_path = Path("results_beam4.jsonl")
+
+with jsonlines.open(baseline_path) as f:
+    baseline_data = [line for line in f]
+testset = data[0]["testset"]
+language_pair = data[0]["language_pair"]
+baseline = [line["comet22"] for line in baseline_data if testset == line["testset"] and language_pair == line["language_pair"]][0]
 
 # Line example:
 # {"testset": "wmt21", "language_pair": "de-en", "seed_no": 0, "method": "MBR with aggregate COMET (1 aggregates from 1024 refs)", "num_aggregates": 1, "num_samples": 1024, "chrf": 57.662579358040325, "cometinho": 60.126039303373545, "comet22": 85.5144088923931, "duration": 489.24234914779663, "transl...
@@ -21,7 +27,7 @@ num_samples = max(num_aggregates)
 
 for k in num_aggregates:
     row = [line for line in data if line["num_aggregates"] == k][0]
-    delta = (row["comet22"] - beam_search)
+    delta = (row["comet22"] - baseline)
     print(f"({int(num_samples/k)},{delta:.5f})", end="")
 print()
 
