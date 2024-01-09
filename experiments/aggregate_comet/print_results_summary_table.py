e7aa033f6add01d8b77da9713fa433e237ef1287
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-09 19:16:41 +0100

    .

--- experiments/aggregate_comet/print_results_summary_table.py
+++ experiments/aggregate_comet/print_results_summary_table.py
@@ -44,7 +44,7 @@ beam_results_path = Path(f"results_beam4.jsonl")
 if beam_results_path.exists():
     with jsonlines.open(beam_results_path) as f:
         data = list(f)
-    results = {result["lang_pair"]: result[metric] for result in data if result["testset"] == testset}
+    results = {result["language_pair"]: result[metric] for result in data if result["testset"] == testset}
     print(f"Beam search (size 4) & {results['en-de']:.1f} & {results['de-en']:.1f} & {results['en-ru']:.1f} & {results['ru-en']:.1f} & tba / - \\\\")
 
 sampling_chrf_results_path = Path(f"results_chrf_{testset}_1024samples_seed0.jsonl")
@@ -52,13 +52,13 @@ sampling_chrf_results_path = Path(f"results_chrf_{testset}_1024samples_seed0.jso
 if sampling_chrf_results_path.exists():
     with jsonlines.open(sampling_chrf_results_path) as f:
         data = list(f)
-    sampling_results = {result["lang_pair"]: result[metric] for result in data if result["testset"] == testset and result["method"] == "Sampling"}
+    sampling_results = {result["language_pair"]: result[metric] for result in data if result["testset"] == testset and result["method"] == "Sampling"}
     print(f"Epsilon sampling (\epsilon=0.02) & {sampling_results['en-de']:.1f} & {sampling_results['de-en']:.1f} & {sampling_results['en-ru']:.1f} & {sampling_results['ru-en']:.1f} & tba / - \\\\")
     print(r"\midrule")
     print(r"MBR with \chrf{} metric & & & & &  \\")
-    chrf_pairwise_results = {result["lang_pair"]: result[metric] for result in data if result["testset"] == testset and result["method"] == "MBR with standard ChrF"}
+    chrf_pairwise_results = {result["language_pair"]: result[metric] for result in data if result["testset"] == testset and result["method"] == "MBR with standard ChrF"}
     print(f"– pairwise & {chrf_pairwise_results['en-de']:.1f} & {chrf_pairwise_results['de-en']:.1f} & {chrf_pairwise_results['en-ru']:.1f} & {chrf_pairwise_results['ru-en']:.1f} & tba / tba \\\\")
-    chrf_aggregate_results = {result["lang_pair"]: result[metric] for result in data if result["testset"] == testset and result["method"] == "MBR with aggregate ChrF"}
+    chrf_aggregate_results = {result["language_pair"]: result[metric] for result in data if result["testset"] == testset and result["method"] == "MBR with aggregate ChrF"}
     print(f"– reference aggregation (factor 1024) & {chrf_aggregate_results['en-de']:.1f} & {chrf_aggregate_results['de-en']:.1f} & {chrf_aggregate_results['en-ru']:.1f} & {chrf_aggregate_results['ru-en']:.1f} & tba / tba \\\\")
 else:
     print(r"Epsilon sampling (\epsilon=0.02) & & & & & tba / - \\")
@@ -76,7 +76,7 @@ for lang_pair in language_pairs:
     if path.exists():
         with jsonlines.open(path) as f:
             data = list(f)
-        results = {result["lang_pair"]: result[metric] for result in data if result["testset"] == testset and result["num_aggregates"] == 1024}
+        results = {result["language_pair"]: result[metric] for result in data if result["testset"] == testset and result["num_aggregates"] == 1024}
         print(f"{results[lang_pair]:.1f} & ", end="")
     else:
         print("tba & ", end="")
@@ -87,7 +87,7 @@ for lang_pair in language_pairs:
     if path.exists():
         with jsonlines.open(path) as f:
             data = list(f)
-        results = {result["lang_pair"]: result[metric] for result in data if result["testset"] == testset and result["num_aggregates"] == 32}
+        results = {result["language_pair"]: result[metric] for result in data if result["testset"] == testset and result["num_aggregates"] == 32}
         print(f"{results[lang_pair]:.1f} & ", end="")
     else:
         print("tba & ", end="")
@@ -102,7 +102,7 @@ for lang_pair in language_pairs:
     if path.exists():
         with jsonlines.open(path) as f:
             data = list(f)
-        results = {result["lang_pair"]: result[metric] for result in data if result["testset"] == testset and result["num_aggregates"] == 1024}
+        results = {result["language_pair"]: result[metric] for result in data if result["testset"] == testset and result["num_aggregates"] == 1024}
         print(f"{results[lang_pair]:.1f} & ", end="")
     else:
         print("tba & ", end="")
@@ -113,7 +113,7 @@ for lang_pair in language_pairs:
     if path.exists():
         with jsonlines.open(path) as f:
             data = list(f)
-        results = {result["lang_pair"]: result[metric] for result in data if result["testset"] == testset and result["num_aggregates"] == 32}
+        results = {result["language_pair"]: result[metric] for result in data if result["testset"] == testset and result["num_aggregates"] == 32}
         print(f"{results[lang_pair]:.1f} & ", end="")
     else:
         print("tba & ", end="")
