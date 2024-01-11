161b2594519de7da6c7209fe2844cd5b87a8b5a3
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-11 17:42:30 +0100

    .

--- experiments/aggregate_comet/run_n_by_s_cometinho.py
+++ experiments/aggregate_comet/run_n_by_s_cometinho.py
@@ -6,7 +6,7 @@ import jsonlines
 import sacrebleu
 from datasets import load_dataset
 
-from utils import run_all_comet_variants
+from utils import mbr_standard_comet
 
 language_pair = sys.argv[1]
 assert language_pair in ["de-en", "en-de", "en-ru", "ru-en"]
@@ -16,7 +16,8 @@ assert split in ["valid", "test"]
 
 seed_no = int(sys.argv[3])
 
-num_samples = 1024
+num_samples = 1024  # N
+num_references = 128  # S; num_samples / num_references corresponds to aggregation factor
 epsilon_cutoff = 0.02
 
 if split == "valid":
@@ -45,7 +46,7 @@ for row in samples:
 unique_sample_counts = [len(set(row)) for row in samples]
 print(f"Average number of unique samples: {sum(unique_sample_counts) / len(unique_sample_counts):.2f}")
 
-results_file = jsonlines.open(Path(__file__).parent / f"results_cometinho_{wmt}_{language_pair}_{num_samples}samples_seed{seed_no}.jsonl", "w")
+results_file = jsonlines.open(Path(__file__).parent / f"results_{num_samples}n_by_{num_references}s_cometinho_{wmt}_{language_pair}_seed{seed_no}.jsonl", "w")
 
 chrf = evaluate.load("chrf")
 cometinho = evaluate.load("comet", "eamt22-cometinho-da")
@@ -58,50 +59,48 @@ references = Path(ref_path).read_text().splitlines()
 source_sequences = dataset["test"]["text"]
 assert len(dataset["test"]) == len(references) == len(source_sequences)
 
-# print("Only using 16 samples for testing")
-# samples = samples[:16]
-# source_sequences = source_sequences[:16]
-# references = references[:16]
+print("Only using 16 samples for testing")
+samples = samples[:16]
+source_sequences = source_sequences[:16]
+references = references[:16]
 
 cometinho.scorer = cometinho.scorer.to("cuda:0")
-translation_lists, durations = run_all_comet_variants(
+translations = mbr_standard_comet(
     cometinho,
     samples=[[row[i] for row in samples] for i in range(len(samples[0]))],
-    references=[[row[i] for row in samples] for i in range(len(samples[0]))],
+    references=[[row[i] for row in samples] for i in range(len(samples[0]))][:num_references],
     inputs=source_sequences,
     batch_size_embed=512,
     batch_size_estimate=512,
 )
 
-for i, (translations, duration) in enumerate(zip(translation_lists, durations)):
-    chrf_score = chrf.compute(
-        predictions=translations,
-        references=references,
-    )
-    cometinho_score = cometinho.compute(
-        predictions=translations,
-        references=references,
-        sources=source_sequences,
-        # gpus=0,
-    )
-    comet22_score = comet.compute(
-        predictions=translations,
-        references=references,
-        sources=source_sequences,
-        # gpus=0,
-    )
-    results_file.write({
-        "testset": wmt,
-        "language_pair": language_pair,
-        "seed_no": seed_no,
-        "method": f"MBR with aggregate COMET ({int(2**i)} aggregates from {num_samples} refs)",
-        "num_aggregates": int(2**i),
-        "num_samples": num_samples,
-        "chrf": chrf_score["score"],
-        "cometinho": 100 * cometinho_score["mean_score"],
-        "comet22": 100 * comet22_score["mean_score"],
-        "duration": duration,
-        "translations": translations,
-    })
+chrf_score = chrf.compute(
+    predictions=translations,
+    references=references,
+)
+cometinho_score = cometinho.compute(
+    predictions=translations,
+    references=references,
+    sources=source_sequences,
+    # gpus=0,
+)
+comet22_score = comet.compute(
+    predictions=translations,
+    references=references,
+    sources=source_sequences,
+    # gpus=0,
+)
+results_file.write({
+    "testset": wmt,
+    "language_pair": language_pair,
+    "seed_no": seed_no,
+    "method": f"MBR N-by-S",
+    "num_samples": num_samples,
+    "num_references": num_references,
+    "chrf": chrf_score["score"],
+    "cometinho": 100 * cometinho_score["mean_score"],
+    "comet22": 100 * comet22_score["mean_score"],
+    "translations": translations,
+})
 
 results_file.close()
