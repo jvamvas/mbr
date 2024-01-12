b0afc6abca44d99fc4b29994ffca6bb2d57f1297
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-12 15:33:35 +0100

    .

--- experiments/aggregate_comet/benchmark_n_by_s_cometinho_all.py
+++ experiments/aggregate_comet/benchmark_n_by_s_cometinho_all.py
@@ -67,13 +67,9 @@ translation_lists, durations = run_all_comet_n_by_s(
 )
 
 print("Total:")
-for i, aggregation_factor in enumerate(([2**i for i in range(1, len(durations))])):
-    if aggregation_factor == 1:
-        continue  # Identical to pairwise
-    print(f"Aggregation factor {aggregation_factor}: {durations[i]:.2f}s")
+for j, num_subsamples in enumerate([2**i for i in range(len(durations) - 1)]):
+    print(f"S = {num_subsamples}: {durations[j]:.2f}s")
 
 print("Average per segment:")
-for i, aggregation_factor in enumerate(([2**i for i in range(1, len(durations))])):
-    if aggregation_factor == 1:
-        continue  # Identical to pairwise
-    print(f"Aggregation factor {aggregation_factor}: {durations[i] / num_segments:.2f}s")
+for j, num_subsamples in enumerate([2**i for i in range(len(durations) - 1)]):
+    print(f"S = {num_subsamples}: {durations[j] / num_segments:.2f}s")
