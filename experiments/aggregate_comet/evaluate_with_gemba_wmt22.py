a9f8ba533c16c1f2e1d6a8e24afd8c5d92f8971c
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-21 20:04:52 +0100

    Add gemba script

---
+++ experiments/aggregate_comet/evaluate_with_gemba_wmt22.py
@@ -0,0 +1,51 @@
+import sys
+from pathlib import Path
+
+import numpy as np
+import sacrebleu
+from tqdm import tqdm
+
+from experiments.aggregate_comet.gemba import CREDENTIALS
+from experiments.aggregate_comet.gemba.cache import Cache
+from experiments.aggregate_comet.gemba.gpt_api import GptApi
+from experiments.aggregate_comet.gemba.prompt import prompts, language_codes
+
+OPENAI_MODEL = "babbage-002"
+
+language_pair = sys.argv[1]
+assert language_pair in ["de-en", "en-de", "en-ru", "ru-en"]
+translations_path = Path(sys.argv[2])
+assert translations_path.exists()
+
+wmt = "wmt22"
+src_path = sacrebleu.get_source_file(wmt, language_pair)
+ref_path = sacrebleu.get_reference_files(wmt, language_pair)[0]
+source_sequences = Path(src_path).read_text().splitlines()
+references = Path(ref_path).read_text().splitlines()
+assert len(source_sequences) == len(references)
+translations = Path(translations_path).read_text().splitlines()
+assert len(source_sequences) == len(translations)
+
+cache_filename = f"{OPENAI_MODEL}_{wmt}_{language_pair}.jsonl"
+cache = Cache(cache_filename)
+
+gptapi = GptApi(CREDENTIALS.credentials)
+gemba_type = "GEMBA-DA_ref"
+
+scores = []
+for source, reference, translation in zip(tqdm(source_sequences), references, translations):
+    data = {
+        "source_seg": source,
+        "target_seg": translation,
+        "reference_seg": reference,
+        "source_lang": language_codes[language_pair.split("-")[0]],
+        "target_lang": language_codes[language_pair.split("-")[1]],
+    }
+    prompt = prompts[gemba_type]["prompt"].format(**data)
+    parsed_answers = gptapi.request(prompt, OPENAI_MODEL, prompts[gemba_type]["validate_answer"], cache=cache)
+    score = parsed_answers[0]["answer"]
+    scores.append(score)
+
+print("Number of segments", len(scores))
+print("NaN rate", sum([1 for score in scores if score is None]) / len(scores))
+print("Average score:", np.nanmean(np.array(scores, dtype=float)))
