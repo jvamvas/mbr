7eaa64cf2ab0f78b80e88395ba672e2a90e4b9f5
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-22 00:28:52 +0100

    Revert "."
    This reverts commit 19acf0b69528a302e6d8495c246bad1dbddd4822.

--- experiments/aggregate_comet/gemba.py
+++ experiments/aggregate_comet/gemba.py
@@ -35,7 +35,7 @@ class GembaMetric:
         """
         cache_dir = Path(os.getenv("GEMBA_CACHE", Path.home() / ".cache" / "gemba" / self.model.replace("/", "_")))
         if not cache_dir.exists():
-            cache_dir.mkdir(parents=True)
+            os.mkdir(cache_dir)
         return cache_dir / (str(self).replace("/", "_") + ".sqlite")
 
     def load_cache(self) -> SqliteDict:
