4d4f98f1b6d48e89ef8a57e7f4917c42ad8de0d4
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-23 11:15:54 +0100

    .

--- experiments/aggregate_comet/extract_references.py
+++ experiments/aggregate_comet/extract_references.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import sacrebleu
 
+# References
 out_dir = Path(__file__).parent / "references"
 out_dir.mkdir(exist_ok=True)
 
@@ -21,3 +22,24 @@ for split in ["valid", "test"]:
         references = Path(ref_path).read_text().splitlines()
         with open(out_dir / f"{testset}_{language_pair}.{language_pair.split('-')[1]}", "w") as f:
             f.write("\n".join(references))
+
+# Sources
+out_dir = Path(__file__).parent / "source_sequences"
+out_dir.mkdir(exist_ok=True)
+
+for split in ["valid", "test"]:
+    language_pairs = ["en-de", "de-en", "en-ru", "ru-en"]
+
+    seed_nos = [0, 1]
+    num_seeds = len(seed_nos)
+
+    if split == "test":
+        testset = "wmt22"
+    else:
+        testset = "wmt21"
+
+    for language_pair in language_pairs:
+        src_path = sacrebleu.get_source_file(testset, language_pair)
+        source_sequences = Path(src_path).read_text().splitlines()
+        with open(out_dir / f"{testset}_{language_pair}.{language_pair.split('-')[0]}", "w") as f:
+            f.write("\n".join(source_sequences))
