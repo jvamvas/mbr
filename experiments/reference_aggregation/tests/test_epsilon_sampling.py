6fd35bed85252a735b0b5ae743012e71d40f4972
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-27 13:33:57 +0100

    .

--- experiments/reference_aggregation/tests/test_epsilon_sampling.py
+++ experiments/reference_aggregation/tests/test_epsilon_sampling.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from experiments.reference_aggregation.fairseq_utils import load_model
+
 
 class EpsilonSamplingTestCase(TestCase):
 
@@ -8,7 +10,18 @@ class EpsilonSamplingTestCase(TestCase):
         self.language_pair = "en-de"
 
     def test_epsilon_sampling(self):
-        ...  # TODO
+        model = load_model(self.language_pair)
+        source_sentence = "This is a test."
+        num_samples = 4
+        # ε=0.02
+        samples = model.sample(num_samples * [source_sentence], seed=42, sampling_epsilon_cutoff=0.02),
+        self.assertEqual(len(samples), num_samples)
+        self.assertIsInstance(samples[0], str)
+        print(samples[0])
+        # ε=0
+        samples = model.sample(num_samples * [source_sentence], seed=42, sampling_epsilon_cutoff=0),
+        self.assertEqual(len(samples), num_samples)
+        self.assertIsInstance(samples[0], str)
 
     def test_extract_translations(self):
         ...  # TODO
