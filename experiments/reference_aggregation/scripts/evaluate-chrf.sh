64554c8d9d68b2b72cd86664d00a7b25376f5309
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-30 18:26:19 +0100

    .

--- experiments/reference_aggregation/scripts/evaluate-chrf.sh
+++ experiments/reference_aggregation/scripts/evaluate-chrf.sh
@@ -5,35 +5,41 @@ cd translations
 # Define the language pairs in an array
 declare -a language_pairs=("en-de" "de-en" "en-ru" "ru-en")
 
-sacrebleu -i wmt17 -l en-de -i output.detok.txt -m bleu chrf ter --chrf-word-order 2
-
 # File to store the output
 output_file="../results/chrf.log"
 
 # Iterate over each language pair
 for lp in "${language_pairs[@]}"
 do
+    echo $lp
+
     # Extract src and tgt from lp
     IFS='-' read -ra ADDR <<< "$lp"
     src=${ADDR[0]}
     tgt=${ADDR[1]}
 
-    # Execute the commands and append the output to the log file
+    echo "baselines"
     sacrebleu wmt22.${lp}.ref.${tgt} -i wmt22.${lp}.beam4.${tgt} -m chrf -b >> $output_file
     sacrebleu wmt22.${lp}.ref.${tgt} -i wmt22.${lp}.epsilon0.02.seed0.${tgt} -m chrf -b >> $output_file
 
+    echo "chrf"
     sacrebleu wmt22.${lp}.ref.${tgt} -i mbr.wmt22.${lp}.pairwise.n1024.epsilon0.02.seed0.chrf.${tgt} -m chrf -b >> $output_file
     sacrebleu wmt22.${lp}.ref.${tgt} -i mbr.wmt22.${lp}.aggregate.n1024.epsilon0.02.seed0.chrf.${tgt} -m chrf -b >> $output_file
     sacrebleu wmt22.${lp}.ref.${tgt} -i mbr.wmt22.${lp}.aggregate_to_fine.top20.n1024.epsilon0.02.seed0.chrf.${tgt} -m chrf -b >> $output_file
 
+    echo "cometinho"
     sacrebleu wmt22.${lp}.ref.${tgt} -i mbr.wmt22.${lp}.pairwise.n1024.epsilon0.02.seed0.cometinho.${tgt} -m chrf -b >> $output_file
     sacrebleu wmt22.${lp}.ref.${tgt} -i mbr.wmt22.${lp}.aggregate.n1024.epsilon0.02.seed0.cometinho.${tgt} -m chrf -b >> $output_file
     sacrebleu wmt22.${lp}.ref.${tgt} -i mbr.wmt22.${lp}.aggregate_to_fine.top20.n1024.epsilon0.02.seed0.cometinho.${tgt} -m chrf -b >> $output_file
 
+    echo "comet22"
     sacrebleu wmt22.${lp}.ref.${tgt} -i mbr.wmt22.${lp}.pairwise.n1024.epsilon0.02.seed0.comet22.${tgt} -m chrf -b >> $output_file
     sacrebleu wmt22.${lp}.ref.${tgt} -i mbr.wmt22.${lp}.aggregate.n1024.epsilon0.02.seed0.comet22.${tgt} -m chrf -b >> $output_file
     sacrebleu wmt22.${lp}.ref.${tgt} -i mbr.wmt22.${lp}.aggregate_to_fine.top20.n1024.epsilon0.02.seed0.comet22.${tgt} -m chrf -b >> $output_file
 
+    echo "coarse-to-fine"
     sacrebleu wmt22.${lp}.ref.${tgt} -i mbr.wmt22.${lp}.coarse_to_fine.top20.n1024.epsilon0.02.seed0.chrf-to-comet22.${tgt} -m chrf -b >> $output_file
     sacrebleu wmt22.${lp}.ref.${tgt} -i mbr.wmt22.${lp}.aggregate_to_fine.top20.n1024.epsilon0.02.seed0.chrf-to-comet22.${tgt} -m chrf -b >> $output_file
+
+    echo
 done
